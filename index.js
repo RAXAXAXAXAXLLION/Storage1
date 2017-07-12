274e7b142e8b598dd0d2933e6b468da7b10417d2
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-07-13 00:17:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-require("socket.io").listen('8888').on('connection',function(socket){socket.on('i_msg',function(e){socket.emit('o_msg',{msg:e.data})})})
+require("socket.io").listen('8888').on('connection',function(socket){socket.on('input',function(e){socket.emit('output',{msg:e.data})})})
