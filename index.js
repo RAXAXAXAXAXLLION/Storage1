206bea2eaf838e6f9f998cc16bcbca5bf6d07f86
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-07-13 16:23:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-require("socket.io").listen('8888').on('connection',function(socket){socket.on('input',function(e){socket.emit('output',{msg:e.data})})})
+require("socket.io").listen(require('http').createServer(function(req,res){
+res.writeHead(200,{"Content-Type":"text/html"})
+require("fs").readFile('slave.htm',(err,data)=>{res.end(data)})
+})).on('connection',function(socket){socket.on('input',function(e){socket.emit('output',{msg:e.data})})})
