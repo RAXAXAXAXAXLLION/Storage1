151eeea2ba8c9f51d388737477a81d63364349db
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-07-15 00:17:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,4 @@ require("fs").readFile('slave.htm',(err,data)=>{res.end(data)})
 }).listen('80'.function(){console.log("Server listening.")})
 ).on('connection',function(socket){socket.id=Math.random()
 socket.on('input',function(e){for(socket.id in socket){
-socket.emit('output',{msg:e.data})
-}})})
+socket.emit('output',{msg:e.data})}})})
