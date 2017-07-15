8ba78d6c7749cb1311b13efa7aa570a2fffa80e7
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-07-15 20:25:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("socket.io").listen(require('http').createServer(function(req,res){
 res.writeHead(200,{"Content-Type":"text/html"})
-require("fs").readFile('slave.htm',(err,data)=>{res.end(data)})
-}).listen('80'.function(){console.log("Server listening.")})
+require("fs").readFile('slave1.htm',(err,data)=>{res.end(data)})
+}).listen('8888',function(){console.log("Server listening.")})
 ).on('connection',function(socket){socket.id=Math.random()
 socket.on('input',function(e){for(socket.id in socket){
-socket.emit('output',{msg:e.data})}})})
+socket.emit('output',{data:e.data})}})})
